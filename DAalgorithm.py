1a05efa9b17488e0016a74446ec422e90f947b08
yamasaki12 <34509103+yamasaki12@users.noreply.github.com>
2019-03-13 19:43:36 +0900

    Update DAalgorithm.py

--- DAalgorithm.py
+++ DAalgorithm.py
@@ -19,7 +19,9 @@ def matching(students, schools):
     poolList = students
     differdAccept = schools
 
+    # 提案してきた学生を受け入れるかどうかを決定するメソッド
     def choice(student, school):
+        # 定員以下の場合、学校は受け入れ、現在の受け入れリストを学校の選好順に並べる。
         if school.capacity > len(school.temporaryList):
             index = school.preference.index(student.name)
             i = 1
@@ -30,6 +32,8 @@ def matching(students, schools):
                 else:
                     i += 1
             return None, school
+        # 定員を上回る場合、提案学生とリストの最下位の学生の選好順位を比較し、受け入れかどうかを決定。
+        # rejectされた学生は、poolListに戻される。
         elif school.preference.index(student.name) < school.preference.index(school.temporaryList[-1].name):
             rejectstudent = school.temporaryList[-1]
             rejectstudent.rejectcount += 1
@@ -47,8 +51,9 @@ def matching(students, schools):
             rejectstudent.rejectcount += 1
             return rejectstudent, school
 
+    # poolListが空になる（全員の配属が決定）するまで提案と受け入れ可否を選択し続ける。
     while len(poolList)>0:
-        school = next(school for school in differdAccept if school.name == poolList[0].tempPref())  
+        school = next(school for school in differdAccept if school.name == poolList[0].tempPref())  # 参考Qiita
         index = schools.index(school) 
         select = choice(poolList[0], school)
         if select[0] == None:
@@ -59,6 +64,7 @@ def matching(students, schools):
             poolList.append(select[0]) 
             differdAccept[index] = school
     
+    # 最終結果を辞書形式で表示
     matching = {} 
     for school in differdAccept:
         i = 0
